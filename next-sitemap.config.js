5b91ebf81dd06be7466b3592ac24711ac1b3d65d
vivekshakya8447 <info@medivisorhealth.com>
2025-09-10 16:29:56 +0530

    update this code
    try sitemap update

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://medivisorindiatreatment.com/", // change this to your real domain
+  generateRobotsTxt: true, // automatically generate robots.txt
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
+};
